1c2f4a360a83221b69fc6e22aafbeb67dbe85dad
Rahul Solanki <rahulsolanki3674@gmail.com>
2026-02-09 13:47:17 +0900

    Upgrade JPG UI4

---
+++ app/compress-pdf/page.tsx
@@ -0,0 +1,8 @@
+export default function CompressPDF() {
+  return (
+    <main style={{ padding: "60px", textAlign: "center" }}>
+      <h1>Compress PDF</h1>
+      <p>Coming soon...</p>
+    </main>
+  );
+}
---
+++ app/merge-pdf/page.tsx
@@ -0,0 +1,8 @@
+export default function MergePDF() {
+  return (
+    <main style={{ padding: "60px", textAlign: "center" }}>
+      <h1>Merge PDF</h1>
+      <p>Coming soon...</p>
+    </main>
+  );
+}
